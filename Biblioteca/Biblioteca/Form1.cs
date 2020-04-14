24c21fd1fbca8128d5d1c54bad063f7e173bbd69
AndreiBacs <63639137+AndreiBacs@users.noreply.github.com>
2020-04-14 18:01:46 +0300

    Add files via upload

---
+++ Biblioteca/Biblioteca/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Biblioteca
+{
+    public partial class WelcomeF : Form
+    {
+        public WelcomeF()
+        {
+            InitializeComponent();
+        }
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AdminF f3 = new AdminF(); // Instantiate a Form3 object.
+            f3.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UserF f2 = new UserF(); // Instantiate a Form3 object.
+            f2.Show();
+        }
+
+        private void WelcomeF_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Biblioteca/Biblioteca/Form4.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Biblioteca
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Addbook ad = new Addbook();
+            ad.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UpdateB ub = new UpdateB();
+            ub.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            RequestedB req = new RequestedB();
+            req.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (Form f in Application.OpenForms.Cast<Form>().ToList())
+                {
+                    if (f.Name != "AdminF")
+                    {
+                        f.Close();
+                      new AdminF().Show();
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Return ret = new Return();
+            ret.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ManUsers manu  =new  ManUsers();
+            manu.Show();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
